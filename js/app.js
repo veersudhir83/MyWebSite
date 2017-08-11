bac0e2882f2aa055079887abb6fb514d06eda8d1
veersudhir83 <veersudhir83@gmail.com>
2017-08-11 17:37:44 +0530

    New content on the page

--- js/app.js
+++ js/app.js
@@ -1,25 +1,33 @@
 var main = function() {
-  /* Push the body and the nav over by 120px over */
-  $('.icon-menu').click(function() {
-    $('.menu').animate({
-      left: "0px"
-    }, 200);
+    /* Push the body and the nav over by 120px over */
+    $('.icon-menu').click(function() {
+        $('.menu').animate({
+            left: "0px"
+        }, 200);
 
-    $('body').animate({
-      left: "120px"
-    }, 200);
-  });
+        $('body').animate({
+            left: "120px"
+        }, 200);
+    });
 
-  /* Then push them back */
-  $('.icon-close').click(function() {
-    $('.menu').animate({
-      left: "-120px"
-    }, 200);
+    /* Then push them back */
+    $('.icon-close').click(function() {
+        $('.menu').animate({
+            left: "-120px"
+        }, 200);
 
-    $('body').animate({
-      left: "0px"
-    }, 200);
-  });
+        $('body').animate({
+            left: "0px"
+        }, 200);
+    });
+
+    $('#ready').click(function() {
+        $('#ready').fadeOut(1000);
+    });
+
+    $('#notready').click(function() {
+        $('#notready').fadeOut(1000);
+    });
 };
 
 
